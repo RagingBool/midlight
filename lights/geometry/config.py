4afd6856b360f662a01ef6bdf94f5c622560c971
Amos Onn <amosonn@gmail.com>
2016-04-04 01:54:58 +0300

    Cleaned config, added FRAME_RATE.

--- lights/geometry/config.py
+++ lights/geometry/config.py
@@ -1,9 +1,16 @@
 
-MATRIX = [
-    ["l00", "l10"],
-    ["l01", "l11"],
-]
+import sys
 
-DEBUG = {
-    "debug1": ["l00", "l10", "l01", "l11"]
-}
+def get_config(pi_id):
+    if pi_id == 1:
+        return {
+            "MATRIX": [
+                ["l00", "l10"],
+                ["l01", "l11"],
+            ],
+            "DEBUG": {
+                "debug1": ["l00", "l10", "l01", "l11"]
+            },
+        }
+
+FRAME_RATE = 2
