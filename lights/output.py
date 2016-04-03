a3a0032c69b039d95a8744a00fc19fb2d34d1686
Amos Onn <amosonn@gmail.com>
2016-04-04 02:03:10 +0300

    Changed output to be aiters.

--- lights/output.py
+++ lights/output.py
@@ -5,9 +5,22 @@ class OutputDevice(object):
     """
     Base class for output devices. These are responsible for serializing the
     light configuration and sending it to the hardware.
+    Should be aiters, which send to hardware on __anext__.
     """
+    def __init__(self, upstreams):
+        self._upstreams = upstreams
+        self._aits = None
+
+    async def __aiter__(self):
+        self._aits = tuple(await aiter(aiterable) for aiterable in \
+            self._upstreams) 
+        return self
     
-    def emit(self):
+    async def __anext__(self):
+        c = tuple(await anext(ait) for ait in self._aits)
+        await self.emit()
+        
+    async def emit(self):
         """
         Serialize and emit via the proper protocol a message for all the lights
         under this output device to change to their new color (or
@@ -21,7 +34,8 @@ class DebugOutputDevice(OutputDevice):
     Output device for debug purposes - prints the current state of the lights.
     """
     
-    def __init__(self, id, lights):
+    def __init__(self, id, lights, upstreams):
+        super().__init__(self, upstreams)
         lights = list(lights)
         for light in lights:
             if not isinstance(light, Light):
@@ -30,7 +44,7 @@ class DebugOutputDevice(OutputDevice):
         self._lights = lights
         self._id = id
 
-    def emit(self):
+    async def emit(self):
         print("Current state:")
         for light in self._lights:
             print("{}: {} = {}".format(self._id, light, light.state))
