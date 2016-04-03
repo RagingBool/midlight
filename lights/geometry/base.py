37f54983fbc9ec5dfb7eb638470d1029a1f923a6
Amos Onn <amosonn@gmail.com>
2016-04-04 01:04:44 +0300

    Extended Geometry to have GeometryState.

--- lights/geometry/base.py
+++ lights/geometry/base.py
@@ -4,8 +4,28 @@ class Geometry(object):
     """
     Base class for geometries of light collections. A Geometry object should:
     Construct with a collection of light objects;
-    Have setters and getters which alter the light-state of these lights;
-    Have an iterator which yields all lights, so their state can pass through
-    some geometry-agnostic filter.
+    Have get_state which gives a matching GeometryState object with all the
+        light states;
+    Have set_state which gets a matchin GeometryState object and sets all the
+        lights to their states.
     """
-    pass
+    STATE_OBJ = GeometryState
+
+    def get_state(self):
+        raise NotImplementedError()
+
+    def set_state(self, geo_state):
+        raise NotImplementedError()
+
+
+class GeometryState(object):
+    """
+    Base class for geometries of light states. Corresponds to a Geometry class,
+    and stores the state of lights under the correct geometry.
+    Should always have a __iter__ method for going of all the lights serially.
+    """
+    def __init__(self):
+        raise NotImplementedError()
+
+    def __iter__(self):
+        raise NotImplementedError()
