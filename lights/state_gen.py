d75f0f6733c12197f881ced4bfd91b74040ddeda
Amos Onn <amosonn@gmail.com>
2016-04-04 01:30:48 +0300

    Made StateGen an aiter.

--- lights/state_gen.py
+++ lights/state_gen.py
@@ -5,20 +5,28 @@ DELTA = "DELTA"
 
 class _StateGen(object):
     """
-    This class, a singleton, is responsible for aggregating the inputs and
-    creating a coherent input state to be used by light managers.
+    This class, a singleton, is an aiter responsible for aggregating the inputs
+    and creating a coherent input state to be used by light managers.
     """
-    def __init__(self):
+    def __init__(self, framerate):
+        if not isinstance(framerate, int):
+            raise TypeError("Framerate should be integral")
         self._init_t = time.time()
         self._accu = 0.0
+        self._framecount = 0
+        self._framerate = framerate
 
-    def render(self):
-        """
-        Create an input state frame.
-        """
+    async def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        left = (self._framecount / self._framerate) + self._init_t - time.time()
+        if left > 0:
+            await asyncio.sleep(left)
         rel = time.time() - self._init_t
         delta = rel - self._accu
         self.accu += delta  # This is to avoid floating error drift.
+        self._framecount += 1
         return {
             DELTA: delta,
         }
