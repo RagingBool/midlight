5a917041c5ab60c218899c8d950a162b26949d00
Amos Onn <amosonn@gmail.com>
2016-04-11 21:23:14 +0300

    Reformed util for better separation of functional and async.

--- lights/util.py
+++ lights/util.py
@@ -1,11 +1,36 @@
 
 
-from aitertools import aiter, anext
 from functools import wraps
+
+from lights.aitertools import aiter, anext
+from lights.geometry.base import GeometryState
+
+
+class Filter(object):
+    """
+    Baseclass for synchronous (functional) filters. Implements a single method,
+    _step, which receives a light state and an input state, and outputs a
+    modified state. May consume members of the input state (mutable), except
+    for the DELTA member.
+    """
+    GEO_CLS = GeometryState
+
+    def step(self, light_state, input_state):
+        if not isinstance(input_state, dict) or \
+            not isinstance(light_state, self.GEO_CLS):
+            raise TypeError()
+        new_light_state = self._step(light_state, input_state)
+        if not isinstance(new_light_state, self.GEO_CLS):
+            raise RuntimeError()
+        return new_light_state
+
+    def _step(self, light_state, input_state):
+        raise NotImplementedError()
+
 
 class _PopIterator(object):
     """
-    Helper class for _Filter.
+    Helper class for _FilterFor.
     """
     def __init__(self):
         self._value = None
@@ -23,50 +48,50 @@ class _PopIterator(object):
     def set_value(self, value):
         self._value = value
 
-class _Filter(object):
+
+def filter_for(geo_cls):
     """
-    Helper class for filter_for.
+    Decorator factory, turning decorated generator into a filter for given
+    geometry class.
     """
-    def __init__(self, geo_cls, upstream, filter, *args, **kwargs):
-        self._geo_cls = geo_cls
+    def _inner(f):
+        class _FilterFor(Filter):
+            GEO_CLS = geo_cls
+        
+            def __init__(self, *args, **kwargs):
+                self._pop_iter = _PopIterator()
+                self._it = iter(f(self._pop_iter, *args, **kwargs))
+        
+            def _step(self, light_state, input_state):
+                self._pop_iter.set_value((light_state, input_state))
+                return next(self._it)
+        
+        return _FilterFor
+    return _inner
+
+
+def is_filter_for(f, geo_cls):
+    if not isinstance(f, Filter):
+        return False
+    if f.geo_cls != geo_cls:
+        return False
+    return True
+
+
+class AsyncAppliedFilter(Filter):
+    """
+    Class for applying a filter to an aiter, getting a new aiter.
+    """
+    def __init__(self, filter, upstream):
+        self._filter = filter
         self._upstream = upstream
         self._ait = None
-        self._pop_iter = _PopIterator()
-        self._filter = iter(filter(self._pop_iter, *args, **kwargs))
 
     async def __aiter__(self):
         self._ait = await aiter(self._upstream)
         return self
 
     async def __anext__(self):
-        geo_state, input_state = await anext(self._ait)
-        if not isinstance(geo_state, self._geo_cls) or \
-            not isinstance(input_state, dict):
-            raise ValueError("Upstream iterator yielded a bad value.")
-        self._pop_iter.set_value((geo_state, input_state))
-        geo_state, input_state = next(self._filter)
-        if not isinstance(geo_state, self._geo_cls) or \
-            not isinstance(input_state, dict):
-            raise ValueError("Filter yielded a bad value.")
-        return geo_state, input_state
-
-
-def filter_for(geo_cls):
-    """
-    Decorator factory, turning decorated generator into a valid aiterator
-    filter for given geometry class.
-    """
-    def _inner(f):
-        @wraps(f)
-        def _inner2(upstream, *args, **kwargs):
-            return _Filter(geo_cls, upstream, f, *args, **kwargs)
-        return _inner2
-    return _inner
-
-
-def is_filter_for(f, geo_cls):
-    if not isinstance(f, _Filter):
-        return False
-    if f._geo_cls != geo_cls:
-        return False
-    return True
+        light_state, input_state = await anext(self._ait)
+        new_light_state = self._filter.step(light_state, input_state)
+        return new_light_state, input_state
