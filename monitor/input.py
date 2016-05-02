f6b70eaaa4ae7a9c609bd2d17dbc1529d59eff55
Amos Onn <amosonn@gmail.com>
2016-05-03 02:15:58 +0300

    Better working example.

--- monitor/input.py
+++ monitor/input.py
@@ -2,13 +2,12 @@
 import asyncio
 
 class LightStateDP(asyncio.DatagramProtocol):
-    def __init__(self, canvas, var):
-        self.canvas = canvas
-        self.var = var
+    def __init__(self, canvases):
+        self.canvases = canvases
 
     def datagram_received(self, data, addr):
-        if self.var.get() != data[0]:
+        if data[0] >= len(self.canvases):
             print("Got {}, exiting".format(data.hex()))
             return
         print("Got {}, drawing".format(data.hex()))
-        self.canvas.create_rectangle(0, 0, 100, 100, fill="#{}".format(data[1:4].hex()))
+        self.canvases[data[0]].create_rectangle(0, 0, 100, 100, fill="#{}".format(data[1:4].hex()))
--- monitor/main.py
+++ monitor/main.py
@@ -18,21 +18,34 @@ async def run_tk(root, interval=0.05):
             raise
 
 
+class CanvasPacker(object):
+    def __init__(self, canvases, ind):
+        self.canvases = canvases
+        self.canvas = canvases[ind]
+
+    def __call__(self):
+        for c in self.canvases:
+            c.pack_forget()
+        self.canvas.pack(side="bottom")
+
+
 def main():
     master = tk.Tk()
     var = tk.IntVar()
-    r1 = tk.Radiobutton(master=master, text="abc", variable=var, value=0)
-    r2 = tk.Radiobutton(master=master, text="def", variable=var, value=1)
+    canvas0 = tk.Canvas(master=master, height=200, width=200)
+    canvas1 = tk.Canvas(master=master, height=200, width=200)
+    canvases = [canvas0, canvas1]
+    r0 = tk.Radiobutton(master=master, text="abc", variable=var, value=0,
+        command=CanvasPacker(canvases, 0))
+    r1 = tk.Radiobutton(master=master, text="def", variable=var, value=1,
+        command=CanvasPacker(canvases, 1))
+    r0.pack(side="top")
     r1.pack(side="top")
-    r2.pack(side="top")
-    canvas = tk.Canvas(master=master, height=200, width=200)
-    canvas.pack(side="bottom")
-    canvas.create_rectangle(0, 0, 100, 100, fill="#123456")
     asyncio.ensure_future(run_tk(master))
     el = asyncio.get_event_loop()
     asyncio.ensure_future(el.create_datagram_endpoint(
-        lambda: LightStateDP(canvas, var),
-        local_addr=("127.0.0.1", 9999)
+        lambda: LightStateDP(canvases),
+        local_addr=("127.0.0.1", 9999),
     ))
     el.run_forever()
 
