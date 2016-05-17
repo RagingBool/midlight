27f9a0d5c112bd70dae1bad3544c9a8ae26982ff
Amos Onn <amosonn@gmail.com>
2016-05-18 01:47:07 +0300

    Changed painters to have state, so we can update our drawings.

--- monitor/input.py
+++ monitor/input.py
@@ -3,27 +3,28 @@ import asyncio
 
 from common.packet import parse, LightPacket
 from common.lights.matrix import MatrixGeometryState
-from monitor.matrix import matrix_painter
+from monitor.matrix import MatrixPainter
 
 
 PAINTERS = {
-    MatrixGeometryState: matrix_painter,
+    MatrixGeometryState: MatrixPainter,
 }
 
 
 class LightStateDP(asyncio.DatagramProtocol):
     def __init__(self, canvases, inputs_map):
-        self.canvases = canvases
-        self.inputs_map = inputs_map
+        self.painters = {
+            (struct_id, state_type): PAINTERS[state_type](canvases[i]) for \
+            (struct_id, state_type), i in inputs_map.items()
+        }
 
     def datagram_received(self, data, addr):
         p = parse(data, [LightPacket])
         if p is None:
             return
-        i = self.inputs_map.get((p.structure_id, type(p.state)))
-        if i is None:
+        painter = self.painters.get((p.structure_id, type(p.state)))
+        if painter is None:
             return
-        canvas = self.canvases[i]
-        PAINTERS[type(p.state)](canvas, p.state)
+        painter.paint(p.state)
 
 
--- monitor/matrix.py
+++ monitor/matrix.py
@@ -1,10 +1,25 @@
 
-def matrix_painter(canvas, matrix_state):
-    # FIXME: we assume canvas size 600x300.
-    edge = min(600/matrix_state.width, 300/matrix_state.height)
-    for (x,y), c in matrix_state.items():
-        canvas.create_rectangle(
-            int(x*edge), int(y*edge),
-            int((x+1)*edge), int((y+1)*edge),
-            fill=str(c),
-        )
+class MatrixPainter(object):
+    def __init__(self, canvas):
+        self._canvas = canvas
+        self._w = None
+        self._h = None
+        self._rects = None
+
+    def paint(self, matrix_state):
+        if self._w != matrix_state.width or self._h != matrix_state.height:
+            self._w = matrix_state.width
+            self._h = matrix_state.height
+            self._rects = {}
+            # FIXME: we assume canvas size 600x300.
+            edge = min(600/matrix_state.width, 300/matrix_state.height)
+            self._canvas.delete("all")
+            for (x,y), c in matrix_state.items():
+                self._rects[x, y] = self._canvas.create_rectangle(
+                    int(x*edge), int(y*edge),
+                    int((x+1)*edge), int((y+1)*edge),
+                    fill=str(c),
+                )
+        else:
+            for (x,y), c in matrix_state.items():
+                self._canvas.itemconfig(self._rects[x, y], fill=str(c))
