a2f87a4a81c01b030a8714d1c671f5681a15d1d6
Amos Onn <amosonn@gmail.com>
2016-05-24 21:30:32 +0300

    Rearranged cards1 ids.

--- common/config/example.py
+++ common/config/example.py
@@ -7,7 +7,7 @@ GEOMETRIES = {
         for y in range(30)
     ]),
     "cards1": CardsGeometry({
-        h: {c: (0, 1, x, y) for x, c in enumerate(HOUSES[h])} \
-        for y, h in enumerate(HOUSES_KEYS)
+        h: {c: (0, 1, x, y) for y, c in enumerate(HOUSES[h]) \
+        } for x, h in enumerate(HOUSES_KEYS)
     }),
 }
