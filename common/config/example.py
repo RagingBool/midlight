9419257f1ef22155796b9a6b3a249004ccadfe6a
Amos Onn <amosonn@gmail.com>
2016-05-24 03:09:25 +0300

    Added a CardsGeometry to example conf.

--- common/config/example.py
+++ common/config/example.py
@@ -1,8 +1,13 @@
 from common.geometry.matrix import MatrixGeometry
+from common.geometry.cards import CardsGeometry, HOUSES, HOUSES_KEYS
 
 GEOMETRIES = {
     "matrix0": MatrixGeometry([
         [(0, 0, x, y) for x in range(60)] \
         for y in range(30)
     ]),
+    "cards0": CardsGeometry({
+        h: {c: (0, 0, x, y) for x, c in enumerate(HOUSES[h])} \
+        for y, h in enumerate(HOUSES_KEYS)
+    }),
 }
