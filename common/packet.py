f8f97ab3a211ac12efa45e94f76a4c4974a45d01
Amos Onn <amosonn@gmail.com>
2016-05-27 05:00:58 +0300

    Cleaned up base packet types.
    Now they are strings, and have an abstraction of their byte
    representation.

--- common/packet.py
+++ common/packet.py
@@ -22,8 +22,15 @@ def parse_input(buf):
     return type, priority, value
 
 
-LIGHT_PACKET = 0
-INPUT_PACKET = 1
+LIGHT_PACKET = "LIGHT"
+INPUT_PACKET = "INPUT"
+
+PACKETS = [
+    LIGHT_PACKET,
+    INPUT_PACKET,
+]
+
+RPACKETS = {v: i for i, v in enumerate(PACKETS)}
 
 PARSERS = {
     LIGHT_PACKET: parse_light,
@@ -37,7 +44,7 @@ def parse(buf, packet_types=None):
     """
     if not isinstance(buf, (bytes, bytearray)):
         raise TypeError("Bad type for buffer.")
-    t = buf[0]
+    t = PACKETS[buf[0]]
     parser = PARSERS[t]
     if packet_types is not None and t not in packet_types:
         return None
@@ -47,7 +54,7 @@ def parse(buf, packet_types=None):
 def serialize_light(light_ids):
     light_ids = tuple(light_ids.ids)
     b = bytearray(1+len(light_ids)*8)
-    b[0] = LIGHT_PACKET
+    b[0] = RPACKETS[LIGHT_PACKET]
     for i, light_id in enumerate(light_ids):
         b[1+8*i:1+8*i+4] = light_id
         b[1+8*i+4:1+8*i+8] = bytes(Lights[ID(light_id)])
