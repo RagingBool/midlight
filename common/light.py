f2cea918584e1d43ea93ae96ef07bcf3b44891ac
Amos Onn <amosonn@gmail.com>
2016-06-07 20:09:59 +0300

    Really fixed FixedDict setattr.

--- common/light.py
+++ common/light.py
@@ -42,7 +42,7 @@ class FixedDict(dict):
             raise ValueError("Invalid key.")
         if not type(value) == type(self[key]):
             raise TypeError("Invalid value type.")
-        super().__setitem__(key, type())
+        super().__setitem__(key, value)
 
 
 Lights = FixedDict()
