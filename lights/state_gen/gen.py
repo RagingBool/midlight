3e0184856b05f7d8a89e4e3d84a80ad6bdad1433
Amos Onn <amosonn@gmail.com>
2016-06-07 21:21:32 +0300

    Added handling of strobe and hue.

--- lights/state_gen/gen.py
+++ lights/state_gen/gen.py
@@ -3,6 +3,7 @@ import time
 import asyncio
 
 from lights.state_gen.consts import DELTA
+from common.packet import parse, INPUT_PACKET, STROBE, HUE, HUE_ALPHA
 
 class StateGen(object):
     """
@@ -16,6 +17,24 @@ class StateGen(object):
         self._accu = 0.0
         self._framecount = 0
         self._framerate = framerate
+        self._strobe = None
+        self._hue = 0.0
+        self._hue_alpha = None
+        self._hue_wd = 0.0
+
+    def dispatch(self, t, priority, value):
+        if t == STROBE:
+            self._strobe = value
+        elif t == HUE:
+            self._hue = value
+            self._hue_wd = self._accu
+        elif t == HUE_ALPHA:
+            if value == 0.0:
+                self._hue_alpha = None
+            else:
+                self._hue_alpha = value
+            self._hue_wd = self._accu
+            
 
     async def __aiter__(self):
         return self
@@ -28,9 +47,17 @@ class StateGen(object):
         delta = rel - self._accu
         self._accu += delta  # This is to avoid floating error drift.
         self._framecount += 1
-        return {
+        d = {
             DELTA: delta,
         }
+        if self._strobe is not None:
+            d[STROBE] = self._strobe
+            self._strobe = None
+        if self._hue_alpha is not None and self._hue_wd > self._accu - 1.0:
+            d[HUE] = self._hue
+            d[HUE_ALPHA] = self._hue_alpha 
+        print("{}  \r".format(d), end="")
+        return d
 
 
 class StateGenDP(asyncio.DatagramProtocol):
