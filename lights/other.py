ad34d4b9f79e6f48f4e9cbade5a7671ce38b9280
Amos Onn <amosonn@gmail.com>
2017-05-27 18:53:22 +0300

    Added color light matching.

--- lights/other.py
+++ lights/other.py
@@ -9,30 +9,55 @@ import time
 from lights.e131client import E1_31DmxUniverse
 
 def main():
-    universe_id = 3
-    component_identifier = "936DA01F-9ABD-4d9d-80C7-02AF85C822A8"
-    e = E1_31DmxUniverse(universe_id=universe_id, component_identifier=component_identifier) 
-    b = bytearray(512)
-    # b[63 + 3*8] = 255
-    b = bytes([255 for i in range(512)])
-    e.send_frame(b)
-    universe_id = 2
-    component_identifier = "936DA01F-9ABD-4d9d-80C7-02AF85C822A8"
-    e = E1_31DmxUniverse(universe_id=universe_id, component_identifier=component_identifier) 
-    b = bytearray(512)
-    # b[63 + 3*8] = 255
-    b = bytes([255 for i in range(512)])
-    e.send_frame(b)
-    universe_id = 1
-    component_identifier = "936DA01F-9ABD-4d9d-80C7-02AF85C822A8"
-    e = E1_31DmxUniverse(universe_id=universe_id, component_identifier=component_identifier) 
-    b = bytearray(512)
-    # b[63 + 3*8] = 255
-    b = bytes([255 for i in range(512)])
-    e.send_frame(b)
-    # while True:
-        # e.send_frame(b)
-        # time.sleep(0.01)
+    while True:
+        b = bytearray(512)
+        universe_id_ks = [(5, 0), (6, 0), (7, 0), (7, 31), (8, 63)]
+        index = 0
+        universe_id, k = universe_id_ks[index]
+        b[k:k+30] = bytes([
+            255, 255, 255,
+            255,   0,   0,
+              0, 255,   0,
+              0, 0,   255,
+              0, 255, 255,
+            255, 0,   255,
+            255, 255,   0,
+            255, 128,   0,
+            128, 255,   0,
+            128, 0,   255,
+        ])
+        component_identifier = "936DA01F-9ABD-4d9d-80C7-02AF85C822A8"
+        e = E1_31DmxUniverse(universe_id=universe_id, component_identifier=component_identifier) 
+        e.send_frame(b)
+        time.sleep(2)
+        # for universe_id_main in range(5, 9):
+            # for j in range(1):
+                # for k in [0, 31, 63]:
+                    # b = bytearray(512)
+                    # b = bytes([255 if i in range(k, k+32) else 0 for i in range(512)])
+                    # for universe_id in range(5, 9):
+                        # component_identifier = "936DA01F-9ABD-4d9d-80C7-02AF85C822A8"
+                        # e = E1_31DmxUniverse(universe_id=universe_id, component_identifier=component_identifier) 
+                        # if universe_id == universe_id_main:
+                            # e.send_frame(b)
+                        # else:
+                            # e.send_frame(blank)
+                    # print(universe_id_main, k)
+                    # time.sleep(2)
+    # universe_id = 2
+    # component_identifier = "936DA01F-9ABD-4d9d-80C7-02AF85C822A8"
+    # e = E1_31DmxUniverse(universe_id=universe_id, component_identifier=component_identifier) 
+    # b = bytearray(512)
+    # # b[63+3*9] = 255
+    # b = bytes([255 for i in range(512)])
+    # e.send_frame(b)
+    # universe_id = 1
+    # component_identifier = "936DA01F-9ABD-4d9d-80C7-02AF85C822A8"
+    # e = E1_31DmxUniverse(universe_id=universe_id, component_identifier=component_identifier) 
+    # b = bytearray(512)
+    # # b[63 + 3*8] = 255
+    # b = bytes([255 for i in range(512)])
+    # e.send_frame(b)
 
     
     # def __init__(self, universe_id, component_identifier, offset, lights):
